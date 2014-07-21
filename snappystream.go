c49f4de5ce0cb0bcb8c78e04e96a890b0aa7e8ba
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-07-20 22:13:31 -0700

    constants from the spec (and godoc)

--- snappystream.go
+++ snappystream.go
@@ -7,6 +7,18 @@ import (
 	"hash/crc32"
 )
 
+// Ext is the file extension for files whose content is a snappy framed stream.
+const Ext = ".sz"
+
+// MediaType is the MIME type used to represent snappy framed content.
+const MediaType = "application/x-snappy-framed"
+
+// ContentEncoding is the appropriate HTTP Content-Encoding header value for
+// requests containing a snappy framed entity body.
+const ContentEncoding = "x-snappy-framed"
+
+// MaxBlockSize is the maximum number of decoded bytes allowed to be
+// represented in a snappy framed block (sections 4.2 and 4.3).
 const MaxBlockSize = 65536
 const VerifyChecksum = true
 const SkipVerifyChecksum = false
